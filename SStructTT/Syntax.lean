536c289594fee9857bdf0fa563f7f8a8e5a153d9
Qiancheng Fu <qcfu@bu.edu>
2025-01-04 19:46:17 -0500

    ok

--- SStructTT/Syntax.lean
+++ SStructTT/Syntax.lean
@@ -1,5 +1,6 @@
 import SStructTT.Subst
 
+section Defs
 variable (Srt : Type)
 
 -- Computational Relevancy
@@ -21,8 +22,10 @@ inductive Tm where
   | id   (A m n : Tm)
   | rfl  (m : Tm)
   | rw   (A h p : Tm)
+end Defs
 
 namespace Tm
+variable {Srt : Type}
 instance : Ids (Tm Srt) where
   ids := var
 
@@ -47,27 +50,27 @@ def rename_rec (ξ : Var -> Var) (m : Tm Srt) : Tm Srt :=
   | rw A m n => rw (rename_rec (upren (upren ξ)) A) (rename_rec ξ m) (rename_rec ξ n)
 
 instance : Rename (Tm Srt) where
-  rename := rename_rec Srt
+  rename := rename_rec
 
-section RenameEqns
+namespace Rename
 variable (ξ : Var -> Var) (A B m n n1 n2 : Tm Srt) (x i : Nat) (r : Rlv) (s : Srt)
 
-@[asimp]lemma rename_ids  : rename ξ (ids x) = @ids (Tm Srt) _ (ξ x) := by rfl
-@[asimp]lemma rename_srt  : rename ξ (srt s i) = srt s i := by rfl
-@[asimp]lemma rename_pi   : rename ξ (pi A B r s) = pi (rename ξ A) (rename (upren ξ) B) r s := by rfl
-@[asimp]lemma rename_lam  : rename ξ (lam A m r s) = lam (rename ξ A) (rename (upren ξ) m) r s := by rfl
-@[asimp]lemma rename_app  : rename ξ (app m n) = app (rename ξ m) (rename ξ n) := by rfl
-@[asimp]lemma rename_sig  : rename ξ (sig A B r s) = sig (rename ξ A) (rename (upren ξ) B) r s := by rfl
-@[asimp]lemma rename_pair : rename ξ (pair m n r s) = pair (rename ξ m) (rename ξ n) r s := by rfl
-@[asimp]lemma rename_proj : rename ξ (proj A m n) = proj (rename (upren ξ) A) (rename ξ m) (rename (upren $ upren ξ) n) := by rfl
-@[asimp]lemma rename_bool : rename ξ (@bool Srt) = bool := by rfl
-@[asimp]lemma rename_tt   : rename ξ (@tt Srt) = tt := by rfl
-@[asimp]lemma rename_ff   : rename ξ (@ff Srt) = ff := by rfl
-@[asimp]lemma rename_ifte : rename ξ (ifte A m n1 n2) = ifte (rename (upren ξ) A) (rename ξ m) (rename ξ n1) (rename ξ n2) := by rfl
-@[asimp]lemma rename_id   : rename ξ (id A m n) = id (rename ξ A) (rename ξ m) (rename ξ n) := by rfl
-@[asimp]lemma rename_rfl  : rename ξ (rfl m) = rfl (rename ξ m) := by rfl
-@[asimp]lemma rename_rw   : rename ξ (rw A m n) = rw (rename (upren $ upren ξ) A) (rename ξ m) (rename ξ n) := by rfl
-end RenameEqns
+@[asimp]lemma ids  : rename ξ (ids x) = @ids (Tm Srt) _ (ξ x) := by rfl
+@[asimp]lemma srt  : rename ξ (srt s i) = srt s i := by rfl
+@[asimp]lemma pi   : rename ξ (pi A B r s) = pi (rename ξ A) (rename (upren ξ) B) r s := by rfl
+@[asimp]lemma lam  : rename ξ (lam A m r s) = lam (rename ξ A) (rename (upren ξ) m) r s := by rfl
+@[asimp]lemma app  : rename ξ (app m n) = app (rename ξ m) (rename ξ n) := by rfl
+@[asimp]lemma sig  : rename ξ (sig A B r s) = sig (rename ξ A) (rename (upren ξ) B) r s := by rfl
+@[asimp]lemma pair : rename ξ (pair m n r s) = pair (rename ξ m) (rename ξ n) r s := by rfl
+@[asimp]lemma proj : rename ξ (proj A m n) = proj (rename (upren ξ) A) (rename ξ m) (rename (upren $ upren ξ) n) := by rfl
+@[asimp]lemma bool : rename ξ (@bool Srt) = bool := by rfl
+@[asimp]lemma tt   : rename ξ (@tt Srt) = tt := by rfl
+@[asimp]lemma ff   : rename ξ (@ff Srt) = ff := by rfl
+@[asimp]lemma ifte : rename ξ (ifte A m n1 n2) = ifte (rename (upren ξ) A) (rename ξ m) (rename ξ n1) (rename ξ n2) := by rfl
+@[asimp]lemma id   : rename ξ (id A m n) = id (rename ξ A) (rename ξ m) (rename ξ n) := by rfl
+@[asimp]lemma rfl  : rename ξ (rfl m) = rfl (rename ξ m) := by rfl
+@[asimp]lemma rw   : rename ξ (rw A m n) = rw (rename (upren $ upren ξ) A) (rename ξ m) (rename ξ n) := by rfl
+end Rename
 
 def subst_rec (σ : Var -> Tm Srt) (m : Tm Srt) : Tm Srt :=
   match m with
@@ -88,39 +91,164 @@ def subst_rec (σ : Var -> Tm Srt) (m : Tm Srt) : Tm Srt :=
   | rw A m n => rw (subst_rec (up $ up σ) A) (subst_rec σ m) (subst_rec σ n)
 
 instance : Subst (Tm Srt) where
-  subst := subst_rec Srt
+  subst := subst_rec
 
-section SubstEqns
+namespace Subst
 variable (σ : Var -> Tm Srt) (A B m n n1 n2 : Tm Srt) (x i : Nat) (r : Rlv) (s : Srt)
 
-@[asimp]lemma subst_ids  : subst σ (ids x) = σ x := by rfl
-@[asimp]lemma subst_srt  : subst σ (srt s i) = srt s i := by rfl
-@[asimp]lemma subst_pi   : subst σ (pi A B r s) = pi (subst σ A) (subst (up σ) B) r s := by rfl
-@[asimp]lemma subst_lam  : subst σ (lam A m r s) = lam (subst σ A) (subst (up σ) m) r s := by rfl
-@[asimp]lemma subst_app  : subst σ (app m n) = app (subst σ m) (subst σ n) := by rfl
-@[asimp]lemma subst_sig  : subst σ (sig A B r s) = sig (subst σ A) (subst (up σ) B) r s := by rfl
-@[asimp]lemma subst_pair : subst σ (pair m n r s) = pair (subst σ m) (subst σ n) r s := by rfl
-@[asimp]lemma subst_proj : subst σ (proj A m n) = proj (subst (up σ) A) (subst σ m) (subst (up $ up σ) n) := by rfl
-@[asimp]lemma subst_bool : subst σ (@bool Srt) = bool := by rfl
-@[asimp]lemma subst_tt   : subst σ (@tt Srt) = tt := by rfl
-@[asimp]lemma subst_ff   : subst σ (@ff Srt) = ff := by rfl
-@[asimp]lemma subst_ifte : subst σ (ifte A m n1 n2) = ifte (subst (up σ) A) (subst σ m) (subst σ n1) (subst σ n2) := by rfl
-@[asimp]lemma subst_id   : subst σ (id A m n) = id (subst σ A) (subst σ m) (subst σ n) := by rfl
-@[asimp]lemma subst_rfl  : subst σ (rfl m) = rfl (subst σ m) := by rfl
-@[asimp]lemma subst_rw   : subst σ (rw A m n) = rw (subst (up $ up σ) A) (subst σ m) (subst σ n) := by rfl
-end SubstEqns
+@[asimp]lemma ids  : subst σ (ids x) = σ x := by rfl
+@[asimp]lemma srt  : subst σ (srt s i) = srt s i := by rfl
+@[asimp]lemma pi   : subst σ (pi A B r s) = pi (subst σ A) (subst (up σ) B) r s := by rfl
+@[asimp]lemma lam  : subst σ (lam A m r s) = lam (subst σ A) (subst (up σ) m) r s := by rfl
+@[asimp]lemma app  : subst σ (app m n) = app (subst σ m) (subst σ n) := by rfl
+@[asimp]lemma sig  : subst σ (sig A B r s) = sig (subst σ A) (subst (up σ) B) r s := by rfl
+@[asimp]lemma pair : subst σ (pair m n r s) = pair (subst σ m) (subst σ n) r s := by rfl
+@[asimp]lemma proj : subst σ (proj A m n) = proj (subst (up σ) A) (subst σ m) (subst (up $ up σ) n) := by rfl
+@[asimp]lemma bool : subst σ (@bool Srt) = bool := by rfl
+@[asimp]lemma tt   : subst σ (@tt Srt) = tt := by rfl
+@[asimp]lemma ff   : subst σ (@ff Srt) = ff := by rfl
+@[asimp]lemma ifte : subst σ (ifte A m n1 n2) = ifte (subst (up σ) A) (subst σ m) (subst σ n1) (subst σ n2) := by rfl
+@[asimp]lemma id   : subst σ (id A m n) = id (subst σ A) (subst σ m) (subst σ n) := by rfl
+@[asimp]lemma rfl  : subst σ (rfl m) = rfl (subst σ m) := by rfl
+@[asimp]lemma rw   : subst σ (rw A m n) = rw (subst (up $ up σ) A) (subst σ m) (subst σ n) := by rfl
+end Subst
 
-section Lemmas
-theorem up_upren (ξ : Var -> Var) :
+section SubstLemmas
+lemma up_upren (ξ : Var -> Var) :
   @up (Tm Srt) _ _ (ren ξ) = ren (upren ξ) := by
   funext x; cases x <;> asimp
 
+lemma rename_subst ξ (m : Tm Srt) : rename ξ m = m.[ren ξ] := by
+  induction m generalizing ξ with
+  | var => asimp
+  | srt => asimp
+  | pi A B r s ihA ihB => asimp[up_upren, ihA, ihB]
+  | lam A m r s ihA ihm => asimp[up_upren, ihA, ihm]
+  | app m n ihm ihn => asimp[ihm, ihn]
+  | sig A B r s ihA ihB => asimp[up_upren, ihA, ihB]
+  | pair m n r s ihm ihn => asimp[ihm, ihn]
+  | proj A m n ihA ihm ihn => asimp[up_upren, ihm, ihA, ihn]
+  | bool => asimp
+  | tt => asimp
+  | ff => asimp
+  | ifte A m n1 n2 ihA ihm ihn1 ihn2 => asimp[up_upren, ihA, ihm, ihn1, ihn2]
+  | id A m n ihA ihm ihn => asimp[ihA, ihm, ihn]
+  | rfl m ihm => asimp[ihm]
+  | rw A m n ihA ihm ihn => asimp[up_upren, ihA, ihm, ihn]
 
+lemma up_ids : up ids = @ids (Tm Srt) _ := by
+  funext x
+  cases x with
+  | zero => asimp
+  | succ => asimp
 
+lemma subst_id (m : Tm Srt) : m.[ids] = m := by
+  induction m with
+  | var => asimp
+  | srt => asimp
+  | pi A B r s ihA ihB => asimp[up_ids, ihA, ihB]
+  | lam A m r s ihA ihm => asimp[up_ids, ihA, ihm]
+  | app m n ihm ihn => asimp[ihm, ihn]
+  | sig A B r s ihA ihB => asimp[up_ids, ihA, ihB]
+  | pair m n r s ihm ihn => asimp[ihm, ihn]
+  | proj A m n ihA ihm ihn => asimp[up_ids, ihm, ihA, ihn]
+  | bool => asimp
+  | tt => asimp
+  | ff => asimp
+  | ifte A m n1 n2 ihA ihm ihn1 ihn2 => asimp[up_ids, ihA, ihm, ihn1, ihn2]
+  | id A m n ihA ihm ihn => asimp[ihA, ihm, ihn]
+  | rfl m ihm => asimp[ihm]
+  | rw A m n ihA ihm ihn => asimp[up_ids, ihA, ihm, ihn]
 
+lemma up_comp_upren (ξ : Var -> Var) (σ : Var -> Tm Srt) :
+  up (ξ !>> σ) = upren ξ !>> up σ := by
+  funext x
+  cases x with
+  | zero => rfl
+  | succ => asimp
 
+lemma ren_subst_comp ξ σ (m : Tm Srt) : m.[ren ξ].[σ] = m.[ξ !>> σ] := by
+  induction m generalizing ξ σ with
+  | var => asimp
+  | srt => asimp
+  | pi A B r s ihA ihB => asimp[up_upren, up_comp_upren, ihA, ihB]
+  | lam A m r s ihA ihm => asimp[up_upren, up_comp_upren, ihA, ihm]
+  | app m n ihm ihn => asimp[ihm, ihn]
+  | sig A B r s ihA ihB => asimp[up_upren, up_comp_upren, ihA, ihB]
+  | pair m n r s ihm ihn => asimp[ihm, ihn]
+  | proj A m n ihA ihm ihn => asimp[up_upren, up_comp_upren, ihm, ihA, ihn]
+  | bool => asimp
+  | tt => asimp
+  | ff => asimp
+  | ifte A m n1 n2 ihA ihm ihn1 ihn2 => asimp[up_upren, up_comp_upren, ihA, ihm, ihn1, ihn2]
+  | id A m n ihA ihm ihn => asimp[ihA, ihm, ihn]
+  | rfl m ihm => asimp[ihm]
+  | rw A m n ihA ihm ihn => asimp[up_upren, up_comp_upren, ihA, ihm, ihn]
 
+lemma up_comp_ren (σ : Var -> Tm Srt) (ξ : Var -> Var) :
+    up σ !>> rename (upren ξ) = up (σ !>> rename ξ)  := by
+  funext x
+  cases x with
+  | zero => asimp
+  | succ n =>
+    asimp[rename_subst]
+    have h1 := ren_subst_comp .succ (ren (upren ξ)) (σ n); asimp at h1
+    have h2 := ren_subst_comp ξ (ren .succ) (σ n); asimp at h2
+    rw[h1, h2]; rfl
 
-end Lemmas
+lemma subst_ren_comp σ ξ (m : Tm Srt) : m.[σ].[ren ξ] = m.[σ !>> rename ξ] := by
+  induction m generalizing σ ξ with
+  | var => asimp[rename_subst]
+  | srt => asimp
+  | pi A B r s ihA ihB => asimp[up_upren, up_comp_ren, ihA, ihB]
+  | lam A m r s ihA ihm => asimp[up_upren, up_comp_ren, ihA, ihm]
+  | app m n ihm ihn => asimp[ihm, ihn]
+  | sig A B r s ihA ihB => asimp[up_upren, up_comp_ren, ihA, ihB]
+  | pair m n r s ihm ihn => asimp[ihm, ihn]
+  | proj A m n ihA ihm ihn => asimp[up_upren, up_comp_ren, ihm, ihA, ihn]
+  | bool => asimp
+  | tt => asimp
+  | ff => asimp
+  | ifte A m n1 n2 ihA ihm ihn1 ihn2 => asimp[up_upren, up_comp_ren, ihA, ihm, ihn1, ihn2]
+  | id A m n ihA ihm ihn => asimp[ihA, ihm, ihn]
+  | rfl m ihm => asimp[ihm]
+  | rw A m n ihA ihm ihn => asimp[up_upren, up_comp_ren, ihA, ihm, ihn]
 
+lemma up_comp (σ τ : Var -> Tm Srt) :  up σ >> up τ = up (σ >> τ) := by
+  apply funext
+  intro x
+  cases x with
+  | zero =>
+    asimp
+  | succ n =>
+    asimp[rename_subst]
+    have h1 := subst_ren_comp τ .succ (σ n)
+    have h2 := ren_subst_comp .succ (up τ) (σ n)
+    rw[h1, h2]
+    rfl
+
+lemma subst_comp (σ τ : Var -> Tm Srt) m : m.[σ].[τ] = m.[σ >> τ] := by
+  induction m generalizing σ τ with
+  | var => asimp
+  | srt => asimp
+  | pi A B r s ihA ihB => asimp[up_comp, ihA, ihB]
+  | lam A m r s ihA ihm => asimp[up_comp, ihA, ihm]
+  | app m n ihm ihn => asimp[ihm, ihn]
+  | sig A B r s ihA ihB => asimp[up_comp, ihA, ihB]
+  | pair m n r s ihm ihn => asimp[ihm, ihn]
+  | proj A m n ihA ihm ihn => asimp[up_comp, ihm, ihA, ihn]
+  | bool => asimp
+  | tt => asimp
+  | ff => asimp
+  | ifte A m n1 n2 ihA ihm ihn1 ihn2 => asimp[up_comp, ihA, ihm, ihn1, ihn2]
+  | id A m n ihA ihm ihn => asimp[ihA, ihm, ihn]
+  | rfl m ihm => asimp[ihm]
+  | rw A m n ihA ihm ihn => asimp[up_comp, ihA, ihm, ihn]
+
+instance : SubstLemmas (Tm Srt) where
+  rename_subst := rename_subst
+  subst_id := subst_id
+  id_subst := by intros; asimp
+  subst_comp := subst_comp
+end SubstLemmas
 end Tm
