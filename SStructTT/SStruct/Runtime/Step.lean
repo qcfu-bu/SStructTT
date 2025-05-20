30f6b7cfbfc1d8659d180f23095796b6363f5452
Qiancheng Fu <qcfu@bu.edu>
2025-05-20 14:06:20 -0400

    ok

--- SStructTT/SStruct/Runtime/Step.lean
+++ SStructTT/SStruct/Runtime/Step.lean
@@ -35,7 +35,7 @@ inductive Drop : Heap Srt -> Tm Srt -> Heap Srt -> Prop where
   | null {H} : Drop H .null H
 
 open Runtime in
-@[simp]def Ctrl.do_drop
+@[simp]def Ctrl.try_drop
     (c : Ctrl)
     (H1 : Heap Srt) (m : Tm Srt)
     (H2 : Heap Srt) (n : Tm Srt) : Prop :=
@@ -45,10 +45,20 @@ open Runtime in
 
 namespace Runtime
 
+@[simp]def HValue (m : Tm Srt) (s : Srt) :=
+  match m with
+  | .lam _ _ s0 => s = s0
+  | .tup (.ptr _) (.ptr _) s0 => s = s0
+  | .tup (.ptr _) .null s0 => s = s0
+  | .tt => s = ord.e
+  | .ff => s = ord.e
+  | _ => False
+
 inductive Step : Heap Srt -> Tm Srt -> Heap Srt -> Tm Srt -> Prop where
-  | lam H m c s l :
+  | alloc H v s l :
     l ∉ H.keys ->
-    Step H (.lam m c s) (H.insert l ⟨.lam m c s, s⟩) (.ptr l)
+    HValue v s ->
+    Step H v (H.insert l ⟨v, s⟩) (.ptr l)
   | app_M H H' m m' n :
     Step H m H' m' ->
     Step H (.app m n) H' (.app m' n)
@@ -60,5 +70,33 @@ inductive Step : Heap Srt -> Tm Srt -> Heap Srt -> Tm Srt -> Prop where
     Step H1 (.app (.ptr l) .null) H2 m.[.null/]
   | beta_ptr H1 H2 H3 m v c s lf lv :
     HLookup H1 lf (.lam m c s) H2 ->
-    c.do_drop H2 (.ptr lv) H3 v ->
+    c.try_drop H2 (.ptr lv) H3 v ->
     Step H1 (.app (.ptr lf) (.ptr lv)) H2 m.[v/]
+  | tup_M H H' m m' n s :
+    Step H m H' m' ->
+    Step H (.tup m n s) H' (.tup m' n s)
+  | tup_N H H' m n n' s :
+    Step H n H' n' ->
+    Step H (.tup m n s) H' (.tup m n' s)
+  | prj_M H H' m m' n c1 c2 :
+    Step H m H' m' ->
+    Step H (.prj m n c1 c2) H' (.prj m' n c1 c2)
+  | prj_elim H1 H2 H3 H4 m1 m2 v1 v2 n c1 c2 s l :
+    HLookup H1 l (.tup m1 m2 s) H2 ->
+    c1.try_drop H2 m1 H3 v1 ->
+    c2.try_drop H3 m2 H4 v2 ->
+    Step H1 (.prj (.ptr l) n c1 c2) H4 n.[v2,v1/]
+  | ite_M H H' m m' n1 n2 :
+    Step H m H' m' ->
+    Step H (.ite m n1 n2) H' (.ite m' n1 n2)
+  | ite_tt H H' n1 n2 l :
+    HLookup H l .tt H' ->
+    Step H (.ite (.ptr l) n1 n2) H' n1
+  | ite_ff H H' n1 n2 l :
+    HLookup H l .ff H' ->
+    Step H (.ite (.ptr l) n1 n2) H' n2
+  | rw_elim H m :
+    Step H (.rw m) H m
+
+notation:50 H:50 " ;; " m:51 " ~>> " H':51 " ;; " m':51 =>
+  Step H m H' m'
