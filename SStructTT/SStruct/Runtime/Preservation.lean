5f093ab66e583d30048ec2cf28334756939f03fe
Qiancheng Fu <qcfu@bu.edu>
2025-05-25 22:22:37 -0400

    ok

--- SStructTT/SStruct/Runtime/Preservation.lean
+++ SStructTT/SStruct/Runtime/Preservation.lean
@@ -256,32 +256,31 @@ lemma Resolved.preservation0' {H1 H2 : Heap Srt} {a b c c' A} :
     . assumption
     . apply Star.SE rd st
 
--- lemma Resolved.preservation1X {H1 H2 H3 H3' : Heap Srt} {a b c c' A} :
---     HMerge H1 H2 H3 -> WR H2 ->
---     [] ;; [] ;; H1 ⊢ a ▷ b ◁ c : A -> Step1 (H3, c) (H3', c') ->
---     ∃ H1', HMerge H1' H2 H3' ∧ [] ;; [] ;; H1' ⊢ a ▷ b ◁ c' : A := by
---   generalize e1: [] = Γ
---   generalize e2: [] = Δ
---   intro mrg0 wr2 ⟨er, rs, wr1⟩ st; induction er generalizing H1 H2 H3 H3' c c'
---   case var wf hs =>
---     subst_vars; cases hs
---   case lam_im A B m m' s sA i lw tyA erm ihm =>
---     subst_vars; cases rs
---     case lam x rsx lwx =>
---       cases st
---       case alloc s l h vl =>
---         cases vl
---         have ⟨h1, h2⟩ := mrg0.split_none h
---         cases ord.contra_dec s with
---         | isTrue h0 =>
---           exists H1.insert l (x.lam s, s); and_intros
---           . apply mrg0.insert_left h
---           . constructor
---             . apply Erased.lam_im <;> assumption
---             . apply Resolve.ptr
---               . simp[HLookup,h0]; aesop
---               . apply Resolve.weaken_insert
---                 constructor <;> assumption
---             . sorry
---         | isFalse _ => sorry
---     case ptr => sorry
+lemma Resolved.preservation1X {H1 H2 H3 H3' : Heap Srt} {a b c c' A} :
+    HMerge H1 H2 H3 -> WR H2 ->
+    [] ;; [] ;; H1 ⊢ a ▷ b ◁ c : A -> Step1 (H3, c) (H3', c') ->
+    ∃ H1', HMerge H1' H2 H3' ∧ [] ;; [] ;; H1' ⊢ a ▷ b ◁ c' : A := by
+  generalize e1: [] = Γ
+  generalize e2: [] = Δ
+  intro mrg0 wr2 ⟨er, rs, wr1⟩ st; induction er generalizing H1 H2 H3 H3' c c'
+  case var wf hs =>
+    subst_vars; cases hs
+  case lam_im A B m m' s sA i lw tyA erm ihm =>
+    subst_vars; cases rs
+    case lam x rsx lwx =>
+      cases st
+      case alloc s l h vl =>
+        cases vl
+        have ⟨h1, h2⟩ := mrg0.split_none h
+        cases ord.contra_dec s with
+        | isTrue h0 =>
+          exists H1.insert l (x.lam s, s); and_intros
+          . sorry
+          . constructor
+            . apply Erased.lam_im <;> assumption
+            . apply Resolve.ptr
+              . assumption
+              . constructor <;> assumption
+            . sorry
+        | isFalse _ => sorry
+    case ptr => sorry
