2e164559b4c6f0cd4e4a8871b013eb2bd09c4e79
Qiancheng Fu <qcfu@bu.edu>
2025-11-13 17:20:21 -0500

    citation for postulate

--- SStructTT/ECC/Normalize.lean
+++ SStructTT/ECC/Normalize.lean
@@ -3,5 +3,11 @@ open ARS
 
 namespace ECC
 
--- Postulate: Martin-Lof Type Theory is strongly normalizing.
+/-
+Postulate: Extended Calculus of Constructions is strongly normalizing.
+
+Luo, Zhaohui (ed.), Computation and Reasoning: A Type Theory for Computer Science
+(Oxford, 1994; online edn, Oxford Academic, 31 Oct. 2023),
+https://doi.org/10.1093/oso/9780198538356.001.0001
+-/
 axiom Typed.normalize {Γ m A} : Γ ⊢ m : A -> SN Step m
